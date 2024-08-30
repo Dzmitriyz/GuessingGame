099c70e62703feccfae6cb7344de68ecd310cbed
Dzmitriyz <hekto2004@mail.ru>
2024-08-30 17:19:34 +0700

    Add Compose fix

--- app/src/main/java/com/example/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/example/guessinggame/ResultFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -18,19 +24,20 @@ import com.example.guessinggame.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
     lateinit var viewModel: ResultViewModel
     lateinit var viewModelFactory: ResultViewModelFactory
-    private var _binding: FragmentResultBinding? = null
-    private val binding get()= _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentResultBinding.inflate(inflater, container, false).apply {
             composeView.setContent {
-                MaterialTheme{
+                MaterialTheme {
                     Surface {
-                        ResultFragmentContent()
+                        view?.let { ResultFragmentContent(it, viewModel) }
                     }
                 }
             }
@@ -42,16 +49,17 @@ class ResultFragment : Fragment() {
 
         binding.resultViewModel = viewModel
 
-        binding.newGameButton.setOnClickListener{
-            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)}
-    return view
+        binding.newGameButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+        }
+        return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
+}
     @Composable
     fun NewGameButton(clicked: () -> Unit){
         Button(onClick = clicked) {
@@ -60,6 +68,18 @@ class ResultFragment : Fragment() {
     }
 
     @Composable
-    fun ResultFragmentContent(){
+    fun ResulText(result:String){
+        Text(text = result, fontSize = 28.sp, textAlign = TextAlign.Center)
     }
-}
+
+    @Composable
+    fun ResultFragmentContent(view: View, viewModel: ResultViewModel){
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            ResulText(viewModel.result)
+            NewGameButton {
+                view.findNavController()
+                    .navigate(R.id.action_resultFragment_to_gameFragment)
+            }
+        }
+    }
